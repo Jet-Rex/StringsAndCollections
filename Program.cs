31af6d692c24a53d8e79723a17d0cd9e1114d598
JetRex <v.bazanov7@gmail.com>
2026-04-02 21:40:57 +0700

    AddFile

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace StringsAndCollections {
+  internal class Program {
+    static void Main(string[] args) {
+      string directoryPath;
+      string[] txtFiles;
+      string filePath;
+      string fileContent;
+      List<string> wrongWords;
+      string phonePattern;
+      string phoneReplacement;
+      var mistakesList = new Dictionary<string, List<string>> {
+        ["привет"] = new List<string> { "првиет", "пирвет", "превед" },
+        ["хорошо"] = new List<string> { "хоршо", "харашо", "хорощо" },
+        ["пока"]   = new List<string> { "поко", "пка", "покка" }
+      };
+      phonePattern = @"\(012\)\s*345-67-89";
+      phoneReplacement = "+380 12 345 67 89";
+
+      Console.Write("Введите путь к папке с файлами: ");
+      directoryPath = Console.ReadLine();
+      //directoryPath = @"C:\Texts";
+      txtFiles = Directory.GetFiles(directoryPath, "*.txt");
+      foreach (string currentFile in txtFiles) {
+        filePath = currentFile;
+        fileContent = File.ReadAllText(filePath);
+        foreach (var pair in mistakesList) {
+          string correctWord = pair.Key;
+          wrongWords = pair.Value;
+          foreach (string wrongWord in wrongWords) {
+            fileContent = fileContent.Replace(wrongWord, correctWord);
+          }
+        }
+        fileContent = Regex.Replace(fileContent, phonePattern, phoneReplacement);
+
+        File.WriteAllText(filePath, fileContent);
+      }
+      Console.WriteLine($"Обработано файлов: {txtFiles.Length}");
+    }
+  }
+}
